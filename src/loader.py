0b13ce8c9b8a87a48e68492fe86e683608d3073f
FlemmingR <146717357+Flemming-R@users.noreply.github.com>
2024-08-17 18:42:25 +0200

    Das war's für heute von mir (wahrscheinlich)

--- src/loader.py
+++ src/loader.py
@@ -9,9 +9,10 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
 from pm4py.visualization.bpmn import visualizer as bpmn_vis
+from typing import Literal
 
-
-log_domestic = xes_importer.apply('Data/DomesticDeclarations.xes')
-log_international = xes_importer.apply('Data/InternationalDeclarations.xes')
-df_domestic = pm4py.convert_to_dataframe(log_domestic)
-df_international = pm4py.convert_to_dataframe(log_international)
+def load_data(logname: Literal['domestic', 'international', 'nonanomaly_Domestic', 'nonanomaly_International']):
+    """Load logdata from path Data/"""
+    log_path = f'Data/{logname.title()}Declarations.xes'
+    log = pm4py.read_xes(log_path)
+    return log
